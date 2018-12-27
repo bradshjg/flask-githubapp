94cbf2e3984c71ac205a744dd66a5813cc454000
= <james.g.bradshaw@gmail.com>
2018-12-27 09:25:31 -0600

    API Tweaks * push the github app context instead of requiring explicitly using the context arg * add trivial tox configuration (still needs tests and wiring) * add support for github secret, still deciding if it should be required (it currenlty is but with poor error handling) * move requirements to setup.py

--- samples/cruel_closer/app.py
+++ samples/cruel_closer/app.py
@@ -5,19 +5,21 @@ from flask_githubapp import GitHubApp
 
 app = Flask(__name__)
 
-app.config['GITHUBAPP_ID'] = os.environ['GITHUBAPP_ID']
+app.config['GITHUBAPP_ID'] = int(os.environ['GITHUBAPP_ID'])
 
 with open(os.environ['GITHUBAPP_KEY_PATH'], 'rb') as key_file:
     app.config['GITHUBAPP_KEY'] = key_file.read()
+
+app.config['GITHUBAPP_SECRET'] = os.environ['GITHUBAPP_SECRET'].encode('utf-8')
 
 github_app = GitHubApp(app)
 
 
 @github_app.on('issues.opened')
-def cruel_closer(context):
-    owner = context.payload['repository']['owner']['login']
-    repo = context.payload['repository']['name']
-    num = context.payload['issue']['number']
-    issue = context.github.issue(owner, repo, num)
+def cruel_closer():
+    owner = github_app.context.payload['repository']['owner']['login']
+    repo = github_app.context.payload['repository']['name']
+    num = github_app.context.payload['issue']['number']
+    issue = github_app.context.github.issue(owner, repo, num)
     issue.create_comment('Could not replicate.')
     issue.close()
