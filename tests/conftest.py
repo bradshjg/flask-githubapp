0534553566c029f6a76d03428beec8bc443cb091
Jimmy Bradshaw <james.g.bradshaw@gmail.com>
2021-01-16 09:09:11 -0600

    Default to using sha256 for payload verification * Support disabling verification via the GITHUBAPP_SECRET setting (set to false to disable)

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,8 @@ import pytest
 
 from flask import Flask
 
+from flask_githubapp import GitHubApp
+
 
 @pytest.fixture
 def app():
@@ -9,4 +11,17 @@ def app():
     app.config['GITHUBAPP_ID'] = 1
     app.config['GITHUBAPP_KEY'] = 'key'
     app.config['GITHUBAPP_SECRET'] = 'secret'
+    return app
+
+@pytest.fixture
+def github_app(app):
+    github_app = GitHubApp(app)
+
+    @github_app.on('issues')
+    def test_issue():
+        return 'issue event'
+
+    @github_app.on('issues.opened')
+    def test_issue_opened():
+        return 'issue opened action'
     return app
--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,20 +1,16 @@
 import os
 
-from flask_githubapp import GitHubApp
-
 FIXURES_DIR = os.path.join(
     os.path.dirname(os.path.abspath(__file__)),
     'fixtures'
 )
 
 
-def test_issues_hook_valid_signature(app):
+def test_issues_hook_valid_legacy_signature(github_app):
     with open(os.path.join(FIXURES_DIR, 'issues_hook.json'), 'rb') as hook:
         issues_data = hook.read()
 
-    GitHubApp(app)
-
-    with app.test_client() as client:
+    with github_app.test_client() as client:
         resp = client.post('/',
                            data=issues_data,
                            headers={
@@ -24,14 +20,28 @@ def test_issues_hook_valid_signature(app):
                            })
         assert resp.status_code == 200
 
-
-def test_issues_hook_invalid_signature(app):
+def test_issues_hook_valid_signature(github_app):
+    """a valid webhook w/ signature should return a 200 response with a valid response payload"""
     with open(os.path.join(FIXURES_DIR, 'issues_hook.json'), 'rb') as hook:
         issues_data = hook.read()
 
-    GitHubApp(app)
+    with github_app.test_client() as client:
+        resp = client.post('/',
+                           data=issues_data,
+                           headers={
+                               'Content-Type': 'application/json',
+                               'X-GitHub-Event': 'issues',
+                               'X-Hub-Signature-256': 'sha256=a4adf8a7da6573f0fbb2f9d43dbe07c7c2e91d27f6ad65bdafae233a88ec0e4b'
+                           })
+        assert resp.status_code == 200
+        assert resp.json == {'calls': {'test_issue': 'issue event', 'test_issue_opened': 'issue opened action'}, 'status': 'HIT'}
 
-    with app.test_client() as client:
+
+def test_issues_hook_invalid_legacy_signature(github_app):
+    with open(os.path.join(FIXURES_DIR, 'issues_hook.json'), 'rb') as hook:
+        issues_data = hook.read()
+
+    with github_app.test_client() as client:
         resp = client.post('/',
                            data=issues_data,
                            headers={
@@ -40,3 +50,51 @@ def test_issues_hook_invalid_signature(app):
                                'X-Hub-Signature': 'sha1=badhash'
                            })
         assert resp.status_code == 400
+
+
+def test_issues_hook_invalid_signature(github_app):
+    with open(os.path.join(FIXURES_DIR, 'issues_hook.json'), 'rb') as hook:
+        issues_data = hook.read()
+
+    with github_app.test_client() as client:
+        resp = client.post('/',
+                           data=issues_data,
+                           headers={
+                               'Content-Type': 'application/json',
+                               'X-GitHub-Event': 'issues',
+                               'X-Hub-Signature': 'sha256=badhash'
+                           })
+        assert resp.status_code == 400
+
+
+def test_issues_hook_missing_signature(github_app):
+    """Return 400 response if the signature is missing and verification is enabled"""
+    with open(os.path.join(FIXURES_DIR, 'issues_hook.json'), 'rb') as hook:
+        issues_data = hook.read()
+
+    with github_app.test_client() as client:
+        resp = client.post('/',
+                           data=issues_data,
+                           headers={
+                               'Content-Type': 'application/json',
+                               'X-GitHub-Event': 'issues',
+                           })
+        assert resp.status_code == 400
+
+
+def test_issues_hook_verification_disabled_missing_signature(github_app):
+    """Return 200 response and valid payload if the signature is missing and verification has been disabled"""
+    with open(os.path.join(FIXURES_DIR, 'issues_hook.json'), 'rb') as hook:
+        issues_data = hook.read()
+
+    github_app.config['GITHUBAPP_SECRET'] = False
+
+    with github_app.test_client() as client:
+        resp = client.post('/',
+                           data=issues_data,
+                           headers={
+                               'Content-Type': 'application/json',
+                               'X-GitHub-Event': 'issues',
+                           })
+        assert resp.status_code == 200
+        assert resp.json == {'calls': {'test_issue': 'issue event', 'test_issue_opened': 'issue opened action'}, 'status': 'HIT'}
